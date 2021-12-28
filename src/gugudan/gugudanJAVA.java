7eb2b6d2470b6faaeebefd8ad3fc40da9b98361c
leezzangmin <ckdals12345678@gmail.com>
2021-12-28 17:39:06 +0900

    구구단 2단 출력 _ 스캐너 사용

--- src/gugudan/gugudanJAVA.java
+++ src/gugudan/gugudanJAVA.java
@@ -1,12 +1,16 @@
 package gugudan;
+import java.util.Scanner;
 
 public class gugudanJAVA {
     public static void main(String[] args)
     {
         //2단
+        Scanner scan= new Scanner(System.in);
+        int dan;
+        dan=scan.nextInt();
         int result;
         for(int i=1;i<10;i++) {
-            result=2*i;
+            result = dan * i;
             System.out.println(result);
 
 
