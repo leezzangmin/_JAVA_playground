91be657f477408a708a5e590d3edbd37b040e146
leezzangmin <ckdals12345678@gmail.com>
2021-12-28 18:14:11 +0900

    구구단 2단 출력 _ 메소드 사용

--- src/gugudan/gugudanJAVA.java
+++ src/gugudan/gugudanJAVA.java
@@ -2,17 +2,23 @@ package gugudan;
 import java.util.Scanner;
 
 public class gugudanJAVA {
+    public static int[] calc(int times){
+        int[] result = new int[9];
+        for(int i=0;i<result.length;i++) {
+            result[i]=times* (i+1);
+        }
+        return result;
+    }
     public static void main(String[] args)
     {
         //2단
         Scanner scan= new Scanner(System.in);
         int dan=scan.nextInt();
-        int[] result=new int[9];
+        int[] result=calc(dan);
         int count = 1;
-        for (int i=0;i<result.length;i++){
-            result[i] = dan * count;
-            count++;
-            System.out.println(result[i]);
+        for(int e:result){
+            System.out.println(e);
+
         }
 
 
