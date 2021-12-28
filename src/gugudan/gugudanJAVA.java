f52ba7804e7c85dbd36023cfd8b12c02666a3115
leezzangmin <ckdals12345678@gmail.com>
2021-12-28 17:34:57 +0900

    구구단 2단 출력 _ 변수사용

--- src/gugudan/gugudanJAVA.java
+++ src/gugudan/gugudanJAVA.java
@@ -4,8 +4,12 @@ public class gugudanJAVA {
     public static void main(String[] args)
     {
         //2단
+        int result;
         for(int i=1;i<10;i++) {
-            System.out.println(2 * i);
+            result=2*i;
+            System.out.println(result);
+
+
         }
     }
 }
