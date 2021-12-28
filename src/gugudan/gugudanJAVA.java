4331bfb473e7319ff62a2f04e2d50175d7e60f58
leezzangmin <ckdals12345678@gmail.com>
2021-12-28 17:29:23 +0900

    구구단 2단 출력 _ for문

--- src/gugudan/gugudanJAVA.java
+++ src/gugudan/gugudanJAVA.java
@@ -4,6 +4,8 @@ public class gugudanJAVA {
     public static void main(String[] args)
     {
         //2단
-        System.out.println(2*1);
+        for(int i=1;i<10;i++) {
+            System.out.println(2 * i);
+        }
     }
 }
